489bfa6c306615c3d3e29aaae14211dd1024aaa0
andrzejewski.d <andrzejewski.d@gmail.com>
2020-09-30 16:38:21 -0400

    added server code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+app.listen(3000, () => {
+	console.log("listening on port 3000");
+});
+
+if (process.env.NODE_ENV == "production") {
+	// if we don't recognize the route, look into the client/build folder
+	// will catch things like main.js and main.css
+	app.use(express.static("client/build"));
+
+	// if there is no route in the client/build folder then:
+	// if we don't regonize the route, serve the html document
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
