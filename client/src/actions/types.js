aaf0e77f30631a4488cbae46a2e0114ed1a36944
andrzejewski.d <andrzejewski.d@gmail.com>
2020-10-22 15:30:12 -0400

    added move section functionality to model in redux

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,9 +7,10 @@ export const CREATE_PAGE_MODEL = "create_page_model";
 export const FETCH_PAGE_MODEL = "fetch_page_model";
 export const UPDATE_PAGE_MODEL = "update_page_model";
 export const UPDATE_REG_PAGE_MODEL = "update_reg_page_model";
-export const ADD_SECTION_REG_PAGE = "ADD_SECTION_REG_PAGE"
+export const ADD_SECTION_REG_PAGE = "ADD_SECTION_REG_PAGE";
+export const MOVE_SECTION = "MOVE_SECTION";
 
 export const ADD_SECTION = "add_section";
 export const UPDATE_SECTION = "update_section";
 
-export const CHANGE_PAGE_EDITOR = "CHANGE_PAGE_EDITOR"
+export const CHANGE_PAGE_EDITOR = "CHANGE_PAGE_EDITOR";
--- client/src/components/designBlockToolbar.js
+++ client/src/components/designBlockToolbar.js
@@ -1,143 +1,154 @@
-import React from 'react'
-import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import SettingsIcon from '@material-ui/icons/Settings';
-import './designBlockToolbar.css';
+import React from "react";
+import { connect } from "react-redux";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import SettingsIcon from "@material-ui/icons/Settings";
+import "./designBlockToolbar.css";
 import Tooltip from "@material-ui/core/Tooltip";
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
 //import DialogContent from '@material-ui/core/DialogContent';
 //import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import Grid from '@material-ui/core/Grid';
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import Grid from "@material-ui/core/Grid";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 import TextField from "@material-ui/core/TextField";
-
-
+import * as actions from "../actions";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
-		width: '600px',
+		width: "600px",
 	},
 	modal: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		outline: 'none',
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		outline: "none",
 	},
 	paper: {
-	  backgroundColor: theme.palette.background.paper,
-	  border: '2px solid #000',
-	  boxShadow: theme.shadows[5],
-	  padding: '0px',
-    },
-    formControl: {
+		backgroundColor: theme.palette.background.paper,
+		border: "2px solid #000",
+		boxShadow: theme.shadows[5],
+		padding: "0px",
+	},
+	formControl: {
 		margin: "20px 0px",
 		minWidth: "100%",
 	},
 }));
 
 function DesignBlockToolbar(props) {
-    const classes = useStyles();
-    const showStreamSettings = props.showStreamSettings;
-    const [deleteConfirmOpen, setDeleteConfirmOpen] = React.useState(false);
-    const [openStreamSettings, setOpenStreamSettings] = React.useState(false);
-    const [content, setContent] = React.useState("wowza");
-    const [wowzaLink, setWowzaLink] = React.useState("");
+	const classes = useStyles();
+	const showStreamSettings = props.showStreamSettings;
+	const [deleteConfirmOpen, setDeleteConfirmOpen] = React.useState(false);
+	const [openStreamSettings, setOpenStreamSettings] = React.useState(false);
+	const [content, setContent] = React.useState("wowza");
+	const [wowzaLink, setWowzaLink] = React.useState("");
 	const [youtubeLink, setYoutubeLink] = React.useState("");
-    const [customHTML, setCustomHTML] = React.useState("");
+	const [customHTML, setCustomHTML] = React.useState("");
 
-    const handleClickDelete = () => {
-        setDeleteConfirmOpen(true);
-    };
+	const handleClickDelete = () => {
+		setDeleteConfirmOpen(true);
+	};
 
-    const handleCloseDelete = () => {
-        setDeleteConfirmOpen(false);
-    };
+	const handleCloseDelete = () => {
+		setDeleteConfirmOpen(false);
+	};
 
-    // Stream Settings:
-    const handleOpenStreamSettings = () => {
+	// Stream Settings:
+	const handleOpenStreamSettings = () => {
 		setOpenStreamSettings(true);
 	};
 
 	const handleCloseStreamSettings = () => {
 		setOpenStreamSettings(false);
-    };
-    
-    const handleChangeContent = (event) => {
+	};
+
+	const handleChangeContent = (event) => {
 		setContent(event.target.value);
-    };
+	};
 
-    const handleChangeWowzaLink = (event) => {
+	const handleChangeWowzaLink = (event) => {
 		setWowzaLink(event.target.value);
-    };
+	};
 
-    const handleChangeYoutubeLink = (event) => {
+	const handleChangeYoutubeLink = (event) => {
 		setYoutubeLink(event.target.value);
-    };
-    
-    const handleChangeCustomHTML = (event) => {
+	};
+
+	const handleChangeCustomHTML = (event) => {
 		setCustomHTML(event.target.value);
 	};
 
-    return(
-        <div>
-            {/* Toolbar */}
-            <div className="toolbar_container">
-                <Tooltip title="Move Up">
-                    <div className="toolbar_button">
-                        <KeyboardArrowUpIcon />
-                    </div>
-                </Tooltip>
-                <Tooltip title="Move Down">
-                    <div className="toolbar_button">
-                        <KeyboardArrowDownIcon />
-                    </div>
-                </Tooltip>
-                <Tooltip title="Delete Design Block">
-                    <div className="toolbar_button" onClick={handleClickDelete}>
-                        <DeleteOutlined />
-                    </div>
-                </Tooltip>
-                {showStreamSettings === "true" && (
-                    <Tooltip title="Stream Settings">
-                        <div className="toolbar_button" onClick={handleOpenStreamSettings}>
-                            <SettingsIcon />
-                        </div>
-                    </Tooltip>
-                )}
-            </div>
+	const handleClickMove = (offset) => {
+		if (
+			props.sectionIndex + offset >= 0 &&
+			props.sectionIndex + offset <= props.maxIndex - 1
+		) {
+			props.moveSection(props.sectionIndex, offset);
+		}
+	};
 
-            {/* Confirm Delete */}
-            <Dialog
-                open={deleteConfirmOpen}
-                onClose={handleCloseDelete}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"Delete design block?"}</DialogTitle>
-                <DialogActions>
-                    <Button onClick={handleCloseDelete} color="primary">
-                        Cancel
-                    </Button>
-                    <Button onClick={handleCloseDelete} color="primary" autoFocus>
-                        Delete
-                    </Button>
-                </DialogActions>
-            </Dialog>
+	return (
+		<div>
+			{/* Toolbar */}
+			<div className="toolbar_container">
+				<Tooltip title="Move Up">
+					<div className="toolbar_button" onClick={() => handleClickMove(-1)}>
+						<KeyboardArrowUpIcon />
+					</div>
+				</Tooltip>
+				<Tooltip title="Move Down">
+					<div className="toolbar_button" onClick={() => handleClickMove(1)}>
+						<KeyboardArrowDownIcon />
+					</div>
+				</Tooltip>
+				<Tooltip title="Delete Design Block">
+					<div className="toolbar_button" onClick={handleClickDelete}>
+						<DeleteOutlined />
+					</div>
+				</Tooltip>
+				{showStreamSettings === "true" && (
+					<Tooltip title="Stream Settings">
+						<div className="toolbar_button" onClick={handleOpenStreamSettings}>
+							<SettingsIcon />
+						</div>
+					</Tooltip>
+				)}
+			</div>
 
-            {/*Stream Settings Modal: */}
-            <Modal
+			{/* Confirm Delete */}
+			<Dialog
+				open={deleteConfirmOpen}
+				onClose={handleCloseDelete}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle id="alert-dialog-title">
+					{"Delete design block?"}
+				</DialogTitle>
+				<DialogActions>
+					<Button onClick={handleCloseDelete} color="primary">
+						Cancel
+					</Button>
+					<Button onClick={handleCloseDelete} color="primary" autoFocus>
+						Delete
+					</Button>
+				</DialogActions>
+			</Dialog>
+
+			{/*Stream Settings Modal: */}
+			<Modal
 				aria-labelledby="transition-modal-title"
 				aria-describedby="transition-modal-description"
 				className={classes.modal}
@@ -146,7 +157,7 @@ function DesignBlockToolbar(props) {
 				closeAfterTransition
 				BackdropComponent={Backdrop}
 				BackdropProps={{
-				timeout: 500,
+					timeout: 500,
 				}}
 				disableAutoFocus={true}
 			>
@@ -155,80 +166,100 @@ function DesignBlockToolbar(props) {
 						<div id="testEmailModal">
 							<h3>Stream Settings</h3>
 							<div className={classes.root}>
-								<Grid container spacing={3} >
+								<Grid container spacing={3}>
 									<Grid item xs={12}>
-                                        <FormControl variant="outlined" className={classes.formControl}>
-                                            {/* Category */}
-                                            <InputLabel id="content">Content</InputLabel>
-                                            <Select
-                                                labelId="content"
-                                                id="content-select"
-                                                required="true"
-                                                value={content}
-                                                onChange={handleChangeContent}
-                                            >
-                                                <MenuItem value={"wowza"}>
-                                                    Wowza
-                                                </MenuItem>
-                                                <MenuItem value={"youtube-live"}>
-                                                    Youtube Live
-                                                </MenuItem>
-                                                <MenuItem value={"custom-embed"}>
-                                                    Custom HTML Embed (Advanced)
-                                                </MenuItem>
-                                            </Select>
-                                        </FormControl>
+										<FormControl
+											variant="outlined"
+											className={classes.formControl}
+										>
+											{/* Category */}
+											<InputLabel id="content">Content</InputLabel>
+											<Select
+												labelId="content"
+												id="content-select"
+												required="true"
+												value={content}
+												onChange={handleChangeContent}
+											>
+												<MenuItem value={"wowza"}>Wowza</MenuItem>
+												<MenuItem value={"youtube-live"}>Youtube Live</MenuItem>
+												<MenuItem value={"custom-embed"}>
+													Custom HTML Embed (Advanced)
+												</MenuItem>
+											</Select>
+										</FormControl>
 									</Grid>
 									<Grid item xs={12}>
-                                        {content === "wowza" && (
-                                            <div>
-                                                <FormControl variant="outlined" className={classes.formControl}>
-                                                    <TextField
-                                                        id="wowza-link"
-                                                        label="Wowza Link"
-                                                        variant="outlined"
-                                                        value={wowzaLink}
-                                                        onChange={handleChangeWowzaLink}
-                                                        placeholder="http://www.wowza.com"
-                                                    />
-                                                </FormControl>
-                                                <p>Need help? Click here for instructions on setting up a Wowza Live stream.</p>
-                                            </div>
-                                        )}
-                                        {content === "youtube-live" && (
-                                            <div>
-                                                <FormControl variant="outlined" className={classes.formControl}>
-                                                    <TextField
-                                                        id="youtube-link"
-                                                        label="Youtube Link"
-                                                        variant="outlined"
-                                                        value={youtubeLink}
-                                                        onChange={handleChangeYoutubeLink}
-                                                        placeholder="http://www.youtube.com"
-                                                    />
-                                                </FormControl>
-                                                <p>Need help? Click here for instructions on setting up a YouTube Live stream.</p>
-                                                <p>Heads up! YouTube may take down any streams containing copyrighted music.</p>
-                                            </div>
-                                        )}
-                                        {content === "custom-embed" && (
-                                            <FormControl variant="outlined" className={classes.formControl}>
-                                                <TextField
-                                                    id="custom-HTML"
-                                                    label="Custom HTML"
-                                                    variant="outlined"
-                                                    multiline
-                                                    rows={12}
-                                                    value={customHTML}
-                                                    onChange={handleChangeCustomHTML}
-                                                />
-                                            </FormControl>
-                                        )}
+										{content === "wowza" && (
+											<div>
+												<FormControl
+													variant="outlined"
+													className={classes.formControl}
+												>
+													<TextField
+														id="wowza-link"
+														label="Wowza Link"
+														variant="outlined"
+														value={wowzaLink}
+														onChange={handleChangeWowzaLink}
+														placeholder="http://www.wowza.com"
+													/>
+												</FormControl>
+												<p>
+													Need help? Click here for instructions on setting up a
+													Wowza Live stream.
+												</p>
+											</div>
+										)}
+										{content === "youtube-live" && (
+											<div>
+												<FormControl
+													variant="outlined"
+													className={classes.formControl}
+												>
+													<TextField
+														id="youtube-link"
+														label="Youtube Link"
+														variant="outlined"
+														value={youtubeLink}
+														onChange={handleChangeYoutubeLink}
+														placeholder="http://www.youtube.com"
+													/>
+												</FormControl>
+												<p>
+													Need help? Click here for instructions on setting up a
+													YouTube Live stream.
+												</p>
+												<p>
+													Heads up! YouTube may take down any streams containing
+													copyrighted music.
+												</p>
+											</div>
+										)}
+										{content === "custom-embed" && (
+											<FormControl
+												variant="outlined"
+												className={classes.formControl}
+											>
+												<TextField
+													id="custom-HTML"
+													label="Custom HTML"
+													variant="outlined"
+													multiline
+													rows={12}
+													value={customHTML}
+													onChange={handleChangeCustomHTML}
+												/>
+											</FormControl>
+										)}
 									</Grid>
-									<Grid item xs={12}id="save-button">
-                                        <button className="Button1" onClick={handleCloseStreamSettings}>
-                                            Save
-                                        </button>
+									<Grid item xs={12} id="save-button">
+										<button
+											className="Button1"
+											onClick={handleCloseStreamSettings}
+										>
+											Save
+										</button>
 									</Grid>
 								</Grid>
 							</div>
@@ -236,8 +267,8 @@ function DesignBlockToolbar(props) {
 					</div>
 				</Fade>
 			</Modal>
-      </div>
-    )
-};
+		</div>
+	);
+}
 
-export default DesignBlockToolbar;
+export default connect(null, actions)(DesignBlockToolbar);
