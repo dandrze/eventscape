6a6c232df94761d96438d6464b3e30308b3e8e4b
Kevin Richardson <kevin.richardson101@gmail.com>
2020-10-26 14:38:40 -0400

    minor css changes

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -7,15 +7,23 @@ export default class Landing extends React.Component {
 		return (
 			<div>
 				<div className="videoBackgroundOverlay">
-				<video playsInline autoPlay loop muted src="https://ak.picdn.net/shutterstock/videos/19772026/preview/stock-footage-bright-colored-led-smd-video-wall-with-high-saturated-patterns-close-up-video.webm" type="video/mp4" className="videoBackground"></video>
+					<video 
+						playsInline 
+						autoPlay 
+						loop 
+						muted 
+						src="https://ak.picdn.net/shutterstock/videos/19772026/preview/stock-footage-bright-colored-led-smd-video-wall-with-high-saturated-patterns-close-up-video.webm" 
+						type="video/webm" 
+						className="videoBackground"
+					></video>
 				</div>
-				<div className="viewportHeader">
+				<header className="viewportHeader">
 					<h2 className="landingHeader">Build a stunning livestream event website.</h2>
 					<h2 className="landingHeader">Get started in seconds.</h2>
 					<Link to="/Create_Account">
 						<button className="Button1 CTAButton">Create Event Website</button>
 					</Link>
-				</div>
+				</header>
 			</div>
 		);
 	}
