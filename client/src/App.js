812abcc12b091c7bcb16ec6c07788e0fd497b8b5
Kevin Richardson <kevin.richardson101@gmail.com>
2020-10-31 22:22:47 -0400

    commented out react router lines for Giller event

--- client/src/App.js
+++ client/src/App.js
@@ -27,17 +27,17 @@ function App() {
 			<ToastContainer position="top-right" autoClose={3000} />
 			<header className="App-header">
 				<Route exact path="/" component={Landing} />
-				<Route exact path="/Dashboard" component={Dashboard} />
-				<Route exact path="/Create_Account" component={Create_Account} />
-				<Route exact path="/Event_Details" component={Event_Details} />
-				<Route exact path="/My_Events" component={My_Events} />
-				<Route exact path="/Design" component={Design} />
-				<Route exact path="/WebsiteSettings" component={WebsiteSettings} />
-				<Route exact path="/Communication" component={Communication} />
-				<Route exact path="/CommunicationEditor" component={EmailEditor} />
-				<Route exact path="/Registrations" component={Registrations} />
-				<Route exact path="/Analytics" component={Analytics} />
-				<Route exact path="/Preview" component={Preview} />
+				{/*<Route exact path="/Dashboard" component={Dashboard} />*/}
+				{/*<Route exact path="/Create_Account" component={Create_Account} />*/}
+				{/*<Route exact path="/Event_Details" component={Event_Details} />*/}
+				{/*<Route exact path="/My_Events" component={My_Events} />*/}
+				{/*<Route exact path="/Design" component={Design} />*/}
+				{/*<Route exact path="/WebsiteSettings" component={WebsiteSettings} />*/}
+				{/*<Route exact path="/Communication" component={Communication} />*/}
+				{/*<Route exact path="/CommunicationEditor" component={EmailEditor} />*/}
+				{/*<Route exact path="/Registrations" component={Registrations} />*/}
+				{/*<Route exact path="/Analytics" component={Analytics} />*/}
+				{/*<Route exact path="/Preview" component={Preview} />*/}
 				<Route exact path="/ScotiabankGillerPrize" component={Giller} />
 
 			</header>
