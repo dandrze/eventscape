2671abb2fae970a5990e751cf59139adb888fea3
andrzejewski.d <andrzejewski.d@gmail.com>
2020-11-05 22:44:19 -0500

    added postgres to eventRoutes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const secure = require("express-force-https");
-const { Client } = require("pg");
 
 require("./models/Event.js");
 
@@ -10,16 +9,6 @@ const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 
 //console.log(process.env.DATABASE_URL);
-
-// Connect to Postgres
-const client = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-});
-
-client.connect();
 
 const app = express();
 const router = express.Router();
@@ -33,12 +22,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(authRoutes);
 app.use(eventRoutes);
-
-app.get("/loaderio-770148bdcbe788892fafba4a049219a4/", async (req, res) => {
-	file = `${__dirname}/public/loaderio-770148bdcbe788892fafba4a049219a4.txt`;
-
-	res.download(file);
-});
 
 if (process.env.NODE_ENV == "production") {
 	// if we don't recognize the route, look into the client/build folder
@@ -54,6 +37,6 @@ if (process.env.NODE_ENV == "production") {
 }
 
 const PORT = process.env.PORT || 5000;
-/*app.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log("listening on port " + PORT);
-});*/
+});
