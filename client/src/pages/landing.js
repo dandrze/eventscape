7be4e3d62badfef89de87fc1d3087a34868c334f
andrzejewski.d <andrzejewski.d@gmail.com>
2020-11-06 16:22:19 -0500

    removed commented out link to create account

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -8,28 +8,24 @@ export default class Landing extends React.Component {
 		return (
 			<div>
 				<div className="videoBackgroundOverlay">
-					<video 
-						playsInline 
-						autoPlay 
-						loop 
-						muted 
-						className="videoBackground"
-					>
+					<video playsInline autoPlay loop muted className="videoBackground">
 						<source src={BGVideo} type="video/mp4"></source>
 					</video>
 				</div>
 				<header className="viewportHeader">
-					<h2 className="landingHeader">Build a stunning livestream event website.</h2>
+					<h2 className="landingHeader">
+						Build a stunning livestream event website.
+					</h2>
 					<h2 className="landingHeader">Get started in seconds.</h2>
 					<p className="coming">Coming Winter 2021</p>
-					{/*<Link to="/Create_Account">
+					<Link to="/Create_Account">
 						<button className="Button1 CTAButton">Create Event Website</button>
-					</Link>*/}
+					</Link>
 				</header>
 			</div>
 		);
 	}
 }
 
-// Background video options: 
+// Background video options:
 // https://ak.picdn.net/shutterstock/videos/19772026/preview/stock-footage-bright-colored-led-smd-video-wall-with-high-saturated-patterns-close-up-video.webm
