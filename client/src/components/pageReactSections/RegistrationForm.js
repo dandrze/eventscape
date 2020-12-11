653d43cf9e299049635e61729e24073c4a0b7814
anrzejewski.d <andrzejewski.d@gmail.com>
2020-12-11 16:16:47 -0500

    froala/react combo form WIP

--- client/src/components/pageReactSections/RegistrationForm.js
+++ client/src/components/pageReactSections/RegistrationForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import FroalaEditor from "react-froala-wysiwyg";
+
 import { ReactFormGenerator } from "../react-form-builder2/lib";
 import AlertModal from "../AlertModal";
 import "./RegistrationForm.css";
@@ -46,8 +48,137 @@ function RegistrationForm(props) {
     }
   };
 
+  const config = {
+    toolbarButtons: {
+      moreRich: {
+        buttons: [
+          "insertImage",
+          "insertVideo",
+          "insertLink",
+          "insertFile",
+          "emoticons",
+          "image",
+        ],
+        buttonsVisible: 5,
+      },
+      moreText: {
+        buttons: [
+          "fontFamily",
+          "fontSize",
+          "bold",
+          "italic",
+          "underline",
+          "strikeThrough",
+          "subscript",
+          "superscript",
+          "textColor",
+          "backgroundColor",
+          "inlineClass",
+          "inlineStyle",
+          "alignLeft",
+          "alignCenter",
+          "alignRight",
+          "alignJustify",
+          "formatOLSimple",
+          "formatOL",
+          "formatUL",
+          "paragraphFormat",
+          "paragraphStyle",
+          "lineHeight",
+          "outdent",
+          "indent",
+          "quote",
+          "clearFormatting",
+        ],
+        buttonsVisible: 0,
+      },
+    },
+    fontFamily: {
+      "Alegreya, serif": "Alegreya",
+      "Archivo Black, sans-serif": "Archivo Black",
+      "Ariel, sans-serif": "Ariel",
+      "Cardo, serif": "Cardo",
+      "Cinzel, serif": "Cinzel",
+      "Enriqueta, serif": "Enriqueta",
+      "'Frank Ruhl Libre', serif": "Frank Ruhl Libre",
+      "'EB Garamond', serif": "EB Garamond",
+      "Georgia, serif": "Georgia",
+      "'Julius Sans One', sans-serif": "Julius Sans One",
+      "Lato, sans-serif": "Lato",
+      "Merriweather, serif": "Merriweather",
+      "Montserrat, sans-serif": "Montserrat",
+      "Nunito, sans-serif": "Nunito",
+      "'Open Sans Condensed',sans-serif": "Open Sans Condensed",
+      "Oswald, sans-serif": "Oswald",
+      "Pacifico, cursive": "Pacifico",
+      "'Playfair Display', serif": "Playfair Display",
+      "Poppins, sans-serif": "Poppins",
+      "Roboto,sans-serif": "Roboto",
+      "Montserrat, sans-serif": "Montserrat",
+      "Rubik, sans-serif": "Rubik",
+      "Spectral, serif": "Spectral",
+      "'Times New Roman', serif": "Times New Roman",
+      "Ubuntu, sans-serif": "Ubuntu",
+      "Yellowtail, cursive": "Yellowtail",
+    },
+    fontFamilySelection: true,
+    toolbarInline: true,
+    toolbarVisibleWithoutSelection: true,
+    //heightMin: "568px",
+    placeholderText: "Type something \n or click inside me",
+    charCounterCount: false,
+    keepFormatOnDelete: true,
+    attribution: false,
+    width: "100%",
+    imageEditButtons: [
+      "imageReplace",
+      "imageAlign",
+      //"imageCaption",
+      "imageRemove",
+      "",
+      "|",
+      "imageLink",
+      "linkOpen",
+      "linkEdit",
+      "linkRemove",
+      "-",
+      "imageDisplay",
+      "imageStyle",
+      "imageAlt",
+      "imageSize",
+    ],
+    key:
+      "gVG3C-8D1F1B4D5A3C1ud1BI1IMNBUMRWAi1AYMSTRBUZYB-16D4E3D2B2C3H2C1B10D3B1==",
+  };
+
+  const model = `<div class="one" contenteditable="true">
+  <p style="margin-top: 0;"><strong><span style="font-size: 30px; text-align: left;">Register For Event</span></strong></p>
+  <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>`;
+
+  const theme = `
+  .form-editor-froala {
+    margin: 5%;
+    text-align: justify;
+}
+
+.form-editor-react  {
+    padding: 8%;
+    margin: 5%;
+    background-color: #EFEFEF;
+}
+  .container {
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+    grid-gap: 1rem;
+
+}
+	
+  `;
   return (
     <div>
+      <style>{theme}</style>
       <AlertModal
         open={open}
         onClose={closeModal}
@@ -56,12 +187,20 @@ function RegistrationForm(props) {
         closeText="Cancel"
         continueText="OK"
       />
-      <ReactFormGenerator
-        action_name={props.registerText || "Register now"}
-        onSubmit={handleSubmit}
-        data={formData}
-        answer_data={props.prePopulatedValues}
-      />
+      <div className="container">
+        <div className="form-editor-froala">
+          <FroalaEditor model={model} config={config} />
+        </div>
+        <div className="form-editor-react">
+          <ReactFormGenerator
+            action_name={props.registerText || "Register now"}
+            onSubmit={handleSubmit}
+            data={formData}
+            answer_data={props.prePopulatedValues}
+            className="form-editor-react"
+          />
+        </div>
+      </div>
     </div>
   );
 }
