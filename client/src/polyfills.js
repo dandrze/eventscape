425bc157cfeea4e71aff4fd25cd0bb54ae8a8f30
Kevin Richardson <kevin.richardson101@gmail.com>
2020-12-26 19:39:31 -0500

    polyfills added

--- client/src/polyfills.js
+++ client/src/polyfills.js
@@ -2,3 +2,4 @@ import 'react-app-polyfill/ie11';
 import 'core-js/features/array/find';
 import 'core-js/features/array/includes';
 import 'core-js/features/number/is-nan';
+import "core-js/stable";
