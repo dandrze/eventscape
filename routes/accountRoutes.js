1c23a7eae448c141c1e771280796565e133b084f
anrzejewski.d <andrzejewski.d@gmail.com>
2020-12-28 18:38:37 -0500

    account endpoint in server

---
+++ routes/accountRoutes.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const db = require("../db");
+
+const router = express.Router();
+
+router.put("/api/account", async (req, res) => {
+  const { userId, contactData } = req.body;
+  const { first_name, last_name, email } = contactData;
+
+  const updatedAccount = await db.query(
+    "UPDATE user_account SET first_name=$1, last_name=$2, email=$3 WHERE id=$4 RETURNING first_name, last_name, email, id",
+    [first_name, last_name, email, userId],
+    (err, res) => {
+      if (err) {
+        throw res.status(500).send(err);
+      }
+    }
+  );
+
+  res.status(200).send(updatedAccount.rows[0]);
+});
+
+router.put("/api/account/pw", async (req, res) => {
+  const { userId, oldPassword, newPassword } = req.body;
+
+  const existingUser = await db.query(
+    "SELECT password from user_account where id=$1",
+    [userId]
+  );
+
+  // if the password doesn't match, return an 401 unauthorized error
+  if (oldPassword != existingUser.rows[0].password) {
+    return res.status(401).send({ error: "Current password is not correct" });
+  }
+
+  const updatedAccount = await db.query(
+    "UPDATE user_account SET password=$1 WHERE id=$2 returning id",
+    [newPassword, userId],
+    (err, res) => {
+      if (err) {
+        throw res.status(500).send(err);
+      }
+    }
+  );
+
+  res.status(200).send(updatedAccount.rows[0]);
+});
+
+module.exports = router;
