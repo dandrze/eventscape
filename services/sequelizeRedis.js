e465c037225fd728783698bca4fc289f84e1c791
anrzejewski.d <andrzejewski.d@gmail.com>
2021-02-12 11:57:44 -0500

    removed logging from sequelize to reduce load on server

--- services/sequelizeRedis.js
+++ services/sequelizeRedis.js
@@ -1,7 +1,17 @@
 const SequelizeRedis = require("sequelize-redis");
-const redisClient = require("./redis");
+const redis = require("redis");
+const bluebird = require("bluebird");
+const keys = require("../config/keys");
 
+// Let's promisify Redis
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const redisClient = redis.createClient(keys.redisUrl);
+
+// Let's start
 const sequelizeRedis = new SequelizeRedis(redisClient);
+console.log("********* NEW CLIENT CREATEDDD ***********");
 
 const clearCache = (key) => {
   redisClient.del(key);
