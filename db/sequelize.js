f19503c749b565f30764e6dece06d83aa9e2d9d2
anrzejewski.d <andrzejewski.d@gmail.com>
2021-02-22 14:42:39 -0500

    turn off sequelize db logging in production

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,22 +1,24 @@
 const Sequelize = require("sequelize");
-/*
+
 var logging;
-if (process.env.NODE_ENV === "development") {
-  logging = console.log;
-} else {
+if (process.env.NODE_ENV === "production") {
   logging = false;
+} else {
+  logging = console.log;
 }
-*/
 
 // Allows for a local postgres db without ssl
-const dialectOptions = process.env.IS_LOCAL ? {} : {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  }
+const dialectOptions = process.env.IS_LOCAL
+  ? {}
+  : {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    };
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions
+  dialectOptions,
+  logging,
 });
 
 module.exports = sequelize;
