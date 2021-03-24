4f1e9015384d5f5e4a461a0ddd6126c9f531c8f0
anrzejewski.d <andrzejewski.d@gmail.com>
2021-03-24 10:02:32 -0400

    updated aws key names to the standard used by the s3 library

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   ipStackKey: process.env.IP_STACK_KEY,
   jwtSecretKey: process.env.JWT_SECRET_KEY,
-  awsAccessKey: process.env.AWS_ACCESS_KEY,
-  awsSecretKey: process.env.AWS_SECRET_KEY,
+  awsAccessKey: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
   newRelicKey: process.env.NEW_RELIC_LICENSE_KEY,
   redisUrl: process.env.REDIS_URL,
 };
