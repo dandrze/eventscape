0c4890c46214527c37ad39f2df1556ce2630d011
anrzejewski.d <andrzejewski.d@gmail.com>
2021-04-09 12:12:45 -0400

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "primary",
+      script: "./server.js",
+      instances: "1",
+      exec_mode: "cluster",
+    },
+    {
+      name: "replica",
+      script: "./server.js",
+      instances: "-1",
+      exec_mode: "cluster",
+    },
+  ],
+};
