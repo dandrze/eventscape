40eb8eba54d073393ccba5177d23427d55084b04
anrzejewski.d <andrzejewski.d@gmail.com>
2021-04-10 12:46:19 -0400

    console log env vars to confirm what we can access from the dyno

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 console.log(`server process is: ${process.pid}`);
+console.log(process.env);
 
 // Force HTTPS
 if (process.env.NODE_ENV === "production") app.use(secure);
