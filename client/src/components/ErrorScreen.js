a9d69cfc51bfbf88a7b74ca6ab5a0877941f8607
anrzejewski.d <andrzejewski.d@gmail.com>
2021-05-20 10:19:48 -0400

    fixes #297

--- client/src/components/ErrorScreen.js
+++ client/src/components/ErrorScreen.js
@@ -11,7 +11,12 @@ export default () => {
         <div className="form-box shadow-border">
           <p>Oops there was an error.</p>
           <p>Let's get you back home.</p>
-          <Link to="/">
+          <Link
+            to={{
+              pathname: "/",
+              key: Math.random() /*a unique key forces a refresh even if the user is currently on the target pathname*/,
+            }}
+          >
             <button className="Button1" style={{ margin: "auto" }}>
               Back to home
             </button>
