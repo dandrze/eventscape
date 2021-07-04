cb4e8fcea76dcb92a2e38951408a523be1795298
anrzejewski.d <andrzejewski.d@gmail.com>
2021-07-04 17:02:28 -0400

    turned off db sync. Added node-prune

--- db/index.js
+++ db/index.js
@@ -101,7 +101,7 @@ Event.hasOne(License);
 //Event.sync({ alter: true });
 //Account.sync({ alter: true });
 
-sequelize.sync({ alter: true });
+//sequelize.sync({ alter: true });
 
 // any models wrapped with sequelizeRedis can be used with redis caching
 module.exports = {
