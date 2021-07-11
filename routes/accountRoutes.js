f60e42c6e4fe4529b9ca6c4f4445708f1bb20972
andrzejewski.d <andrzejewski.d@gmail.com>
2021-07-11 13:50:19 -0400

    removed bcrypt

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 
 const { Account, Event } = require("../db").models;
 const { clearCache } = require("../services/sequelizeRedis");
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,4 @@
 const passport = require("passport");
-const bcrypt = require("bcrypt");
 
 const LocalStrategy = require("passport-local").Strategy;
 const { Account, AccountCached } = require("../db").models;
