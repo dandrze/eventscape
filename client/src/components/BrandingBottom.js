c1f9815231086779c571ff737bd2c53afa3f0039
andrzejewski.d <andrzejewski.d@gmail.com>
2021-07-15 17:30:37 -0400

    fixes #423

--- client/src/components/BrandingBottom.js
+++ client/src/components/BrandingBottom.js
@@ -7,7 +7,11 @@ import EventscapeLogo from "../icons/eventscape-logo-black.svg";
 export default function BrandingBottom(props) {
   return (
     <div className="branding-bottom-container">
-      <a href="https://eventscape.io" style={{ margin: "0 auto" }}>
+      <a
+        href="https://eventscape.io"
+        target="_blank"
+        style={{ margin: "0 auto" }}
+      >
         <p className="powered-by">Powered by Eventscape</p>
       </a>
     </div>
