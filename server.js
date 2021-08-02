43b9ef239d29df6257f91a60c4f61bf33d061c5e
andrzejewski.d <andrzejewski.d@gmail.com>
2021-08-02 13:42:51 -0400

    added headers to server.js

--- server.js
+++ server.js
@@ -48,6 +48,21 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 // passport set up for user auth
 app.use(bodyParser.json());
 app.use(
