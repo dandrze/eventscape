a3c2527c44d8690ed5e461b0248ed9c2bdea78c3
andrzejewski.d <andrzejewski.d@gmail.com>
2021-08-04 12:15:21 -0400

    added cors manually

--- server.js
+++ server.js
@@ -45,7 +45,13 @@ var corsOptions = {
 //app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://app.emeryhill.com");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+
   next();
 });
 
